a492af0e1519c8f693d0d199ba50f08ee68bc460
Shivam Singh <sjtsexperience@gmail.com>
2023-07-04 21:25:31 +0530

    fiixe-address

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -3,9 +3,12 @@ import Logo from './Logo'
 import { ChevronRight, FacebookIcon, Instagram, Linkedin, TwitterIcon } from 'lucide-react'
 import { nanoid } from 'nanoid'
 import Link from 'next/link'
+import { api } from '@/utils/api'
+import { ContactData } from '@/schema/contactConfig.schema'
+import { Separator } from '@radix-ui/react-separator'
 
 
-const FooterSection = ({title, children}: PropsWithChildren<{title: string}>) => {
+const FooterSection = ({ title, children }: PropsWithChildren<{ title: string }>) => {
     return (
         <div>
             <h4 className='font-bold text-primary-foreground mb-4'>{title}</h4>
@@ -15,25 +18,27 @@ const FooterSection = ({title, children}: PropsWithChildren<{title: string}>) =>
 }
 
 const Footer = () => {
+
+    const { data } = api.contactInfo.getAll.useQuery<ContactData[], ContactData[]>({}, { refetchOnWindowFocus: false });
     const socialLink = [
         {
-            icon: <FacebookIcon className='w-6 h-6 '/>,
+            icon: <FacebookIcon className='w-6 h-6 ' />,
             link: 'https://www.facebook.com/nkpays/',
             name: 'facbook'
         },
         {
-            icon: <TwitterIcon className='w-6 h-6 '/>,
+            icon: <TwitterIcon className='w-6 h-6 ' />,
             link: 'https://twitter.com/NKPAYS',
             name: 'twitter'
         },
         {
-            icon: <Linkedin className='w-6 h-6 '/>,
-            link: 'https://www.instagram.com/nkpayspvtltd/',
+            icon: <Linkedin className='w-6 h-6 ' />,
+            link: 'https://www.linkedin.com/in/nkpayspvtltd/',
             name: 'linkedin'
         },
         {
-            icon: <Instagram className='w-6 h-6'/>,
-            link: 'https://www.linkedin.com/in/nkpayspvtltd/',
+            icon: <Instagram className='w-6 h-6' />,
+            link: 'https://www.instagram.com/nkpayspvtltd/',
             name: 'instagram'
         },
     ];
@@ -74,58 +79,72 @@ const Footer = () => {
             link: 'refund-policy'
         }
     ]
-  return (
-    <footer className='px-4 py-10 bg-black text-white'>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 justify-items-center">
-            <div className='space-y-2 max-w-xs'>
-                <Logo/>
-                <p>Simplify transactions with NKPays – your trusted partner for seamless payments and services.</p>
+    return (
+        <footer className='px-4 py-10 bg-black text-white'>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-10 justify-items-center">
+                <div className='space-y-2 max-w-xs'>
+                    <Logo />
+                    <p>Simplify transactions with NKPays – your trusted partner for seamless payments and services.</p>
 
-                <div className='flex gap-2'>
-                    {
-                        socialLink.map(sc => {
-                            return <a href={sc.link} key={nanoid()} className='rounded-lg border border-primary hover:bg-primary text-primary hover:text-primary-foreground transition-all p-1'>{sc.icon}</a>
-                        })
-                    }
+                    <div className='flex gap-2'>
+                        {
+                            socialLink.map(sc => {
+                                return <a href={sc.link} key={nanoid()} className='rounded-lg border border-primary hover:bg-primary text-primary hover:text-primary-foreground transition-all p-1'>{sc.icon}</a>
+                            })
+                        }
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-2 w-full'>
+                    <FooterSection title="Quick Links">
+                        {
+                            quickLinks.map(ql => {
+                                return <Link className='text-gray-400 hover:text-gray-200 transition-all flex items-center gap-2' href={ql.link} key={nanoid()}>
+                                    <span><ChevronRight /></span>
+                                    <span className='text-sm'>{ql.lable}</span>
+                                </Link>
+                            })
+                        }
+                    </FooterSection>
+                    <FooterSection title="Legal Links">
+                        {
+                            legals.map(ql => {
+                                return <Link className='text-gray-400 hover:text-gray-200 transition-all flex items-center gap-2' href={ql.link} key={nanoid()}>
+                                    <span><ChevronRight /></span>
+                                    <span className='text-sm'>{ql.lable}</span>
+                                </Link>
+                            })
+                        }
+                    </FooterSection>
+                </div>
+
+                <div>
+                    <FooterSection title='Address'>
+                        {
+                            data?.map(d => {
+                                return <div key={nanoid()} className="space-y-2 text-sm text-gray-400 mt-2 border-b border-b-gray-700 last:border-b-0">
+                                    <p className='font-semibold'>{d.title}</p>
+                                    <p className='text-gray-400 max-w-xs'>{d.address.addr}, {d.address.state}, {d.address.country} - {d.address.zipcode}</p>
+                                    {
+                                        d.email?.map(email => {
+                                            return <p key={nanoid()} className='font-semibold'>{email}</p>
+                                        })
+                                    }
+                                    {
+                                        d.phone?.map(phone => {
+                                            return <p key={nanoid()} className='font-semibold'>{phone}</p>
+                                        })
+                                    }
+                                    {/* <p className='font-semibold'>+91-6003884457</p> */}
+                                    <Separator />
+                                </div>
+                            })
+                        }
+                    </FooterSection>
                 </div>
             </div>
-
-            <div className='grid grid-cols-2 w-full'>
-                <FooterSection title="Quick Links">
-                    {
-                        quickLinks.map(ql => {
-                            return <Link className='text-gray-400 hover:text-gray-200 transition-all flex items-center gap-2' href={ql.link} key={nanoid()}>
-                                <span><ChevronRight/></span>
-                                <span className='text-sm'>{ql.lable}</span>
-                                </Link>
-                        })
-                    }
-                </FooterSection>
-                <FooterSection title="Legal Links">
-                    {
-                        legals.map(ql => {
-                            return <Link className='text-gray-400 hover:text-gray-200 transition-all flex items-center gap-2' href={ql.link} key={nanoid()}>
-                                <span><ChevronRight/></span>
-                                <span className='text-sm'>{ql.lable}</span>
-                                </Link>
-                        })
-                    }
-                </FooterSection>
-            </div>
-
-            <div>
-                <FooterSection title='Address'>
-                    <div className="spac-y-2 text-sm text-gray-400">
-                    <p className='font-semibold'>NK Pays</p>
-                    <p className='text-gray-400 max-w-xs'>Titabar Na Ali, Opst Titabar Public Club, Jorhat, Assam- 785630</p>
-                    <p className='font-semibold'>snkpays@gmail.com</p>
-                    <p className='font-semibold'>+91-6003884457</p>
-                    </div>
-                </FooterSection>
-            </div>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
 
 export default Footer
